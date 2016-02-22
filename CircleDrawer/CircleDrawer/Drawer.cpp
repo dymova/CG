50c94f5218b153e2bccc4d588347bf350343d487
Dymova Anastasiya <adimova05@gmail.com>
2016-02-22 20:48:55 +0600

    new draw algorithm

--- CircleDrawer/CircleDrawer/Drawer.cpp
+++ CircleDrawer/CircleDrawer/Drawer.cpp
@@ -23,17 +23,15 @@ void Drawer::drawCircle(QImage* image, Circle* c)
 	QColor blueColor(0, 204, 204);
 
 
-	for (int y = y0-r; y <= y0+r; y++)
+	for (int y = y0 - r; y <= y0 + r; y++)
 	{
-		for (int x = x0-r; x <= x0+r; x++)
+		int bound = (int)(sqrt((double)(r*r - (y - y0) * (y - y0))));
+		for (int x = -bound + x0; x <= bound + x0; x++)
 		{
-			if (((x-x0) * (x-x0)) + ((y-y0) * (y-y0)) <= (r * r))
-			{
 				drawPoint(image, x, y, blueColor);
-			}
 		}
 	}
-	
+
 }
 
 void Drawer::drawPoint(QImage* image, int x, int y, QColor color)
@@ -45,11 +43,11 @@ void Drawer::drawPoint(QImage* image, int x, int y, QColor color)
 	const int BYTES_IN_PIXEL = 3;
 
 	newX = (image->bytesPerLine() / 2) + x*BYTES_IN_PIXEL;
-	newY = (image->height() / 2) + y;
+	newY = (image->height() / 2) - y;
 
 	if (newX <= (image->bytesPerLine() - 3) && newY < (image->height()) && newX >=0 && newY >= 0 )
 	{
-		point = image->bits() + ((image->height() - newY) * image->bytesPerLine()) + newX;
+		point = image->bits() + ((newY) * image->bytesPerLine()) + newX;
 
 		*point = color.red();
 		*++point = color.green();
